b81433d06bb2cee1c140d408b7ff8fff3ef29a28
torako-san <100730549+torako-san@users.noreply.github.com>
2022-10-21 11:47:08 +0900

    エンティティクラスを生成

---
+++ src/main/java/com/example/demo/Entity/Report.java
@@ -0,0 +1,41 @@
+package com.example.demo.Entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "report")
+@Getter
+@Setter
+public class Report {
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String content;
+	
+	@Column(name = "created_date", insertable = false, updatable = false)
+	private Date createdDate;
+	
+	@Column(name = "updated_date", insertable = false, updatable = false)
+	private Date updateDate;
+	
+	@PreUpdate
+	public void onPrePersit() {
+		Date date = new Date();
+		this.updateDate = date;
+	}
+	
+}
