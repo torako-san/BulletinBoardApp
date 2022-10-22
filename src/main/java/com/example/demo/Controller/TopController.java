82ef78ea8d021071828d83813a83b0da658b85df
torako-san <100730549+torako-san@users.noreply.github.com>
2022-10-22 09:27:04 +0900

    コントローラーに投稿処理を記述

---
+++ src/main/java/com/example/demo/Controller/TopController.java
@@ -0,0 +1,65 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.Entity.Report;
+import com.example.demo.Service.TopService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Controller
+public class TopController {
+	
+	@Autowired
+	TopService topService;
+	
+	@GetMapping
+	public ModelAndView top( ) {
+		ModelAndView mav = new ModelAndView();
+		// 投稿を全件取得
+		List<Report> contentData = topService.findAllReport();
+		//画面遷移先を指定
+		mav.setViewName("/top");
+		//投稿データオブジェクトを保管
+		mav.addObject("contents", contentData);
+		
+		return mav;
+	
+	}
+	
+	@PostMapping("/add")
+	@ResponseBody
+	public String addContent(@RequestParam String content) {
+		
+		Report report = new Report();
+		
+//		report.setContent(content);
+		// 投稿をテーブルに格納
+		topService.saveReport(report);
+		
+		// コメントを取得
+		Report LatestPort = topService.findSelectedPost();
+		
+		return GetJson(LatestPort);
+			
+	}
+	
+	private String GetJson(Report LatestPost) {
+		String retVal = null;
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			retVal = objectMapper.writeValueAsString(LatestPost);
+		} catch (JsonProcessingException e) {
+			System.err.println(e);
+		}
+		return retVal;
+	}
+}
